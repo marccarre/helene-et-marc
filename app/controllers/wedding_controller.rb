cab005e7e7fda2c3608e33844021d60b27eb308f
marccarre <carre.marc@gmail.com>
2013-09-22 22:12:23 +0100

    REST-like URLs and proper redirection/routes.

---
+++ app/controllers/wedding_controller.rb
@@ -0,0 +1,31 @@
+class WeddingController < ApplicationController
+  def home
+  end
+
+  def story
+  end
+
+  def photos
+  end
+
+  def program
+  end
+
+  def rsvp
+  end
+
+  def transports
+  end
+
+  def stay
+  end
+
+  def area
+  end
+
+  def gifts
+  end
+
+  def contact
+  end
+end
