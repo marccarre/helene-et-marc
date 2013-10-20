bfd3d3326854467df3e77146e09202d5cefd5103
marccarre <carre.marc@gmail.com>
2013-10-20 09:27:07 +0100

    Fixed mailer to pass tests.

--- app/mailers/wedding/rsvp_mailer.rb
+++ app/mailers/wedding/rsvp_mailer.rb
@@ -1,21 +1,14 @@
 class Wedding::RsvpMailer < ActionMailer::Base
   FROM = ["les.carreguiners@gmail.com"]
-  #CC = ["carre.marc@gmail.com", "queguiner.helene@gmail.com"]
-  CC = ["carre.marc@gmail.com"]
+  CC = ["carre.marc@gmail.com", "queguiner.helene@gmail.com"]
   private_constant :FROM, :CC
 
   default from: FROM
   default cc: CC
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.rsvp_mailer.rsvp_submitted.subject
-  #
   def rsvp_confirmation(booking)
     @booking = booking
-    #to = @booking.address.email || (FROM + CC)
-    to = "carre.marc@gmail.com"
+    to = @booking.address.email || (FROM + CC)
     mail to: to, subject: t("rsvp_mailer.rsvp_submitted.subject")
   end
 end
