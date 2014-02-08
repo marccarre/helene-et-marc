55ee3ec96e3b924688c55339c8e35b760835c022
Marc Carré <carre.marc@gmail.com>
2014-02-08 00:28:55 +0000

    Added validation on total number of passengers when adding one.

--- app/models/wedding/passenger.rb
+++ app/models/wedding/passenger.rb
@@ -8,6 +8,8 @@ module Wedding
     }
 
     validates :car, presence: true
+    validate :validate_car_passengers_count_is_within_limits, on: :create
+
     validates :first_name, presence: true
     validates :family_name, presence: true
     validates :email, allow_blank: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "should be a valid email address" }
@@ -25,5 +27,11 @@ module Wedding
     def to_s
       return to_json.to_s
     end
+
+    private
+      def validate_car_passengers_count_is_within_limits
+        return if car.blank?
+        errors.add(:car, I18n.t('errors.messages.car_is_full')) if car.passengers.size >= car.available_seats
+      end
   end
 end
